e4c42f471eb8703058fd9fcba6cc370b7938571c
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-03-31 17:39:54 +0200

    added initialization of pos taggers to the init file so that it will be loaded only once

---
+++ profiler16_un/taggers/__init__.py
@@ -0,0 +1,14 @@
+from profiler16_un.taggers.polyglot_pos_tagger import PolyglotPOSTagger
+
+
+pos_tagger_en = PolyglotPOSTagger(lang='en')
+pos_tagger_nl = PolyglotPOSTagger(lang='nl')
+pos_tagger_es = PolyglotPOSTagger(lang='es')
+
+def pos_tags(text='', lang='en'):
+    if 'en' == lang:
+        return pos_tagger_en.pos_tags(text)
+    if 'es' == lang:
+        return pos_tagger_es.pos_tags(text)
+    if 'nl' == lang:
+        return pos_tagger_nl.pos_tags(text)
