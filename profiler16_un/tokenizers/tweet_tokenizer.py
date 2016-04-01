59efd5515e15a42501ceca20d28115a09d8d8a27
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-04-01 14:19:37 +0200

    added tokenizer for tweets

---
+++ profiler16_un/tokenizers/tweet_tokenizer.py
@@ -0,0 +1,38 @@
+import re
+
+
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>',  # HTML tags
+    r'(?:@[\w_]+)',  # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)",  # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',  # URLs
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)',  # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])",  # words with - and '
+    r'(?:[\w_]+)',  # other words
+    r'(?:\S)'  # anything else
+]
+
+
+class TweetTokenizer(object):
+    def __init__(self):
+        self.tokens_re = re.compile(r'(' + '|'.join(regex_str) + ')', re.VERBOSE | re.IGNORECASE)
+        self.emoticon_re = re.compile(r'^' + emoticons_str + '$', re.VERBOSE | re.IGNORECASE)
+
+    def tokenize(self, doc):
+        return self.tokens_re.findall(doc)
+
+    def preprocess(self, doc, lowercase=False):
+        tokens = self.tokenize(doc)
+        if lowercase:
+            tokens = [token if self.emoticon_re.search(token) else token.lower() for token in tokens]
+        return tokens
+
+    def __call__(self, doc):
+        return self.preprocess(doc)
