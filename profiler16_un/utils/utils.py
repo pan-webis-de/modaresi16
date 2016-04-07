237c4c7da0191525e4cfa13353dfb51942b6f8f1
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-04-07 14:32:56 +0200

    parameter tuning for gradient boosting

--- profiler16_un/utils/utils.py
+++ profiler16_un/utils/utils.py
@@ -1,5 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+import xgboost as xgb
 
 
 def get_classifier(method='logistic_regression'):
@@ -15,3 +16,11 @@ def get_classifier(method='logistic_regression'):
                                       bootstrap=False,
                                       n_jobs=-1,
                                       random_state=123)
+
+    if 'gradient_boosting' == method:
+        return xgb.XGBClassifier(max_depth=10,
+                                 subsample=0.8,
+                                 n_estimators=500,
+                                 min_child_weight=0.05,
+                                 colsample_bytree=0.2,
+                                 learning_rate=0.1)
