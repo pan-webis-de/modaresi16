96a827cb466527ddfe78f97785dc2517e2c455b1
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-04-08 13:04:33 +0200

    unigrams, bigrams and trigrams added to the profiler

--- profiler16_un/profilers/en_gender_profiler.py
+++ profiler16_un/profilers/en_gender_profiler.py
@@ -10,15 +10,46 @@ from sklearn.pipeline import Pipeline
 from ..tokenizers.tweet_tokenizer import TweetTokenizer
 from sklearn.pipeline import FeatureUnion
 from ..utils.utils import get_classifier
+from ..preprocessors.text_cleaner import TextCleaner
+from sklearn.feature_selection import SelectFromModel
+from sklearn.svm import LinearSVC
+from sklearn import linear_model, decomposition
+from sklearn.decomposition import TruncatedSVD
+
+
+tc = TextCleaner(lowercase=True,
+                 filter_urls=True,
+                 filter_mentions=True,
+                 filter_hashtags=True,
+                 alphabetic=True,
+                 strip_accents=True)
 
 
 class EnglishGenderProfiler():
-    def __init__(self, lang='en', min_n=1, max_n=1, method='logistic_regression'):
-        self.ngram_tokens = ('ngram_tokens', CountVectorizer(min_df=1,
-                                                             lowercase=True,
-                                                             ngram_range=(1, 2),
-                                                             tokenizer=TweetTokenizer()
-                                                             ))
+    def __init__(self, lang='en', min_n=1, max_n=1, method=None):
+        unigrams = ('unigrams', CountVectorizer(min_df=1,
+                                                     tokenizer=TweetTokenizer(),
+                                                     stop_words='english',
+                                                     preprocessor=tc,
+                                                     ngram_range=(1, 1)
+                                                     ))
+        bigrams = ('bigrams', CountVectorizer(min_df=1,
+                                                   tokenizer=TweetTokenizer(),
+                                                   stop_words='english',
+                                                   preprocessor=tc,
+                                                   ngram_range=(2, 2)
+                                                   ))
+        trigrams = ('trigrams', CountVectorizer(min_df=1,
+                                                     tokenizer=TweetTokenizer(),
+                                                     stop_words='english',
+                                                     preprocessor=TextCleaner(lowercase=True,
+                                                                              filter_urls=True,
+                                                                              filter_mentions=True,
+                                                                              filter_hashtags=True,
+                                                                              alphabetic=True,
+                                                                              strip_accents=True),
+                                                     ngram_range=(3, 3)
+                                                     ))
         ngram_chars = ('char_ngrams', Pipeline([
                                                ('vect2', CountVectorizer(min_df=1,
                                                                          analyzer='char',
@@ -26,9 +57,11 @@ class EnglishGenderProfiler():
                                                                          ngram_range=(3, 3), max_features=20000)),
                                                ]))
 
-        self.pipeline = Pipeline([('features', FeatureUnion([self.ngram_tokens])),
+        self.pipeline = Pipeline([('features', FeatureUnion([unigrams, bigrams])),
                                   ('tfidf', TfidfTransformer(sublinear_tf=True)),
-                                  ('chi', SelectPercentile(chi2, percentile=95)),
+                                  # ('chi', SelectKBest(f_classif, k=1000)),
+                                  # ('feature_selection', SelectFromModel(get_classifier(method=method))),
+                                  # ('pca', TruncatedSVD(n_components=300, random_state=42)),
                                   ('lr', get_classifier(method=method))])
 
     def most_informative_feature_for_class(self, vectorizer, classifier, classlabel, n=50):
@@ -38,16 +71,17 @@ class EnglishGenderProfiler():
         for coef, feat in topn:
             print classlabel, feat.encode('utf-8'), coef
 
-    def show_most_informative_features(self, vectorizer, clf, n=20):
+    def show_most_informative_features(self, vectorizer, clf, n=50):
+        print 'Total number of features: {}'.format(len(vectorizer.get_feature_names()))
         feature_names = vectorizer.get_feature_names()
         coefs_with_fns = sorted(zip(clf.coef_[0], feature_names))
         top = zip(coefs_with_fns[:n], coefs_with_fns[:-(n + 1):-1])
         for (coef_1, fn_1), (coef_2, fn_2) in top:
-            print "\t%.4f\t%-15s\t\t%.4f\t%-15s" % (coef_1, fn_1.encode('utf-8'), coef_2, fn_2.encode('utf-8'))
+            print "\t%-15s\t\t%-15s" % (fn_1.encode('utf-8'), fn_2.encode('utf-8'))
 
     def train(self, X_train, Y_train):
         self.model = self.pipeline.fit(X_train, Y_train)
-        self.show_most_informative_features(self.pipeline.named_steps['features'], self.pipeline.named_steps['lr'])
+        # self.show_most_informative_features(self.pipeline.named_steps['features'], self.pipeline.named_steps['lr'])
 
     def predict(self, X):
         return self.model.predict(X)
