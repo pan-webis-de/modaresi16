443a169b9aa8ae1bd3c59b48b6cd5e7ea0307a68
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-04-11 07:25:05 +0200

    moved methods to show the most informative features to the utils package

--- profiler16_un/profilers/en_gender_profiler.py
+++ profiler16_un/profilers/en_gender_profiler.py
@@ -10,6 +10,7 @@ from sklearn.pipeline import Pipeline
 from ..tokenizers.tweet_tokenizer import TweetTokenizer
 from sklearn.pipeline import FeatureUnion
 from ..utils.utils import get_classifier
+from ..utils.utils import show_most_informative_features
 from ..preprocessors.text_cleaner import TextCleaner
 from sklearn.feature_selection import SelectFromModel
 from sklearn.svm import LinearSVC
@@ -30,17 +31,16 @@ tc = TextCleaner(lowercase=True,
 
 class EnglishGenderProfiler():
     def __init__(self, lang='en', min_n=1, max_n=1, method=None):
-        unigrams = ('unigrams', CountVectorizer(min_df=2,
-                                                binary=True,
+        unigrams = ('unigrams', CountVectorizer(min_df=1,
                                                 tokenizer=TweetTokenizer(),
                                                 stop_words='english',
                                                 preprocessor=tc,
                                                 ngram_range=(1, 1)
                                                 ))
-        bigrams = ('bigrams', CountVectorizer(min_df=2,
+        bigrams = ('bigrams', CountVectorizer(min_df=1,
                                               tokenizer=TweetTokenizer(),
                                               preprocessor=tc,
-                                              ngram_range=(2, 3)
+                                              ngram_range=(2, 2)
                                               ))
         ngram_chars = ('char_ngrams', Pipeline([
                                                ('vect2', CountVectorizer(min_df=1,
@@ -58,24 +58,10 @@ class EnglishGenderProfiler():
                                   #                  scoring='accuracy', verbose=1)),
                                   ('lr', get_classifier(method=method))])
 
-    def most_informative_feature_for_class(self, vectorizer, classifier, classlabel, n=50):
-        labelid = list(classifier.classes_).index(classlabel)
-        feature_names = vectorizer.get_feature_names()
-        topn = sorted(zip(classifier.coef_[labelid], feature_names))[-n:]
-        for coef, feat in topn:
-            print classlabel, feat.encode('utf-8'), coef
-
-    def show_most_informative_features(self, vectorizer, clf, n=50):
-        print 'Total number of features: {}'.format(len(vectorizer.get_feature_names()))
-        feature_names = vectorizer.get_feature_names()
-        coefs_with_fns = sorted(zip(clf.coef_[0], feature_names))
-        top = zip(coefs_with_fns[:n], coefs_with_fns[:-(n + 1):-1])
-        for (coef_1, fn_1), (coef_2, fn_2) in top:
-            print "\t%-15s\t\t%-15s" % (fn_1.encode('utf-8'), fn_2.encode('utf-8'))
 
     def train(self, X_train, Y_train):
         self.model = self.pipeline.fit(X_train, Y_train)
-        self.show_most_informative_features(self.pipeline.named_steps['features'], self.pipeline.named_steps['lr'])
+        show_most_informative_features(self.pipeline.named_steps['features'], self.pipeline.named_steps['lr'])
 
     def predict(self, X):
         return self.model.predict(X)
