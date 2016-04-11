fcb22df8cf486881a3f255bcfe370679246fb0d4
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-04-11 07:20:46 +0200

    added a method to get stopwords for a specific language

--- profiler16_un/utils/utils.py
+++ profiler16_un/utils/utils.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 import xgboost as xgb
+from nltk.corpus import stopwords
 
 
 def get_classifier(method='logistic_regression'):
@@ -24,3 +25,23 @@ def get_classifier(method='logistic_regression'):
                                  min_child_weight=0.05,
                                  colsample_bytree=0.3,
                                  learning_rate=0.1)
+
+def most_informative_feature_for_class(vectorizer, classifier, classlabel, n=50):
+    labelid = list(classifier.classes_).index(classlabel)
+    feature_names = vectorizer.get_feature_names()
+    topn = sorted(zip(classifier.coef_[labelid], feature_names))[-n:]
+    for coef, feat in topn:
+        print classlabel, feat.encode('utf-8'), coef
+
+def show_most_informative_features(vectorizer, clf, n=50):
+    print 'Total number of features: {}'.format(len(vectorizer.get_feature_names()))
+    feature_names = vectorizer.get_feature_names()
+    coefs_with_fns = sorted(zip(clf.coef_[0], feature_names))
+    top = zip(coefs_with_fns[:n], coefs_with_fns[:-(n + 1):-1])
+    for (coef_1, fn_1), (coef_2, fn_2) in top:
+        print "\t%-15s\t\t%-15s" % (fn_1.encode('utf-8'), fn_2.encode('utf-8'))
+
+def get_stopwords(languages=['english', 'dutch', 'spanish']):
+    result = []
+    [result.extend(stopwords.words(lang)) for lang in languages]
+    return result
