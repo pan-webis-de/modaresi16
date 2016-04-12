aa354a72264053eed81677db819091ce84e60731
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-04-12 17:14:10 +0200

    organized imports

--- profiler16_un/profilers/en_gender_profiler.py
+++ profiler16_un/profilers/en_gender_profiler.py
@@ -1,10 +1,14 @@
-from ..pipelines.pipelines import word_unigrams, word_bigrams, avg_spelling_error, pos_distribution, char_ngrams
+from ..pipelines.pipelines import avg_spelling_error
+from ..pipelines.pipelines import pos_distribution
+from ..pipelines.pipelines import word_unigrams
+from sklearn.feature_selection import f_classif
+from ..pipelines.pipelines import word_bigrams
+from ..pipelines.pipelines import char_ngrams
 from sklearn.preprocessing import Normalizer
 from sklearn.feature_selection import chi2
-from sklearn.feature_selection import f_classif
-from sklearn.pipeline import Pipeline
 from sklearn.pipeline import FeatureUnion
 from ..utils.utils import get_classifier
+from sklearn.pipeline import Pipeline
 
 
 class EnglishGenderProfiler():
