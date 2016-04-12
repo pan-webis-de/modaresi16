3d5fd7bed380db0fa8dfb166752c7a548004f061
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-04-12 14:24:51 +0200

    finished the xml output generator

--- profiler16_un/datasets/output_generator.py
+++ profiler16_un/datasets/output_generator.py
@@ -2,10 +2,10 @@ import xml.etree.cElementTree as ET
 
 def generate_output(path, filename, author_id, doc_type, lang, age_group, gender):
     root = ET.Element("author")
-    doc = ET.SubElement(root, "doc")
-
-    ET.SubElement(doc, "field1", name="blah").text = "some value1"
-    ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-
+    root.set('id', author_id)
+    root.set('type', doc_type)
+    root.set('lang', lang)
+    root.set('age_group', age_group)
+    root.set('gender', gender)
     tree = ET.ElementTree(root)
     tree.write("filename.xml")
