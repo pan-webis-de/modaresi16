028d85933c0406d0b741322f175e0a79d606430b
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-04-12 15:51:53 +0200

    refactored the punctuation ngram profiler

--- profiler16_un/pipelines/punctuation_ngrams.py
+++ profiler16_un/pipelines/punctuation_ngrams.py
@@ -1,8 +1,22 @@
-def punctuation_ngrams():
-    pprorcessor = TextCleaner(filter_urls=True, filter_mentions=True, filter_hashtags=True, only_punctuation=True, lowercase=False)
-    vectorizer = CountVectorizer(min_df=1, preprocessor=pprcessor, analyzer='char', ngram_range=(10, 10))
-    punctuation_ngrams = ('punctuation_ngrams', Pipeline([('vect', vectorizer),
-                                                          ('tfidf', TfidfTransformer(sublinear_tf=True)),
-                                                          ('scale', Normalizer())]))
-    return punctuation_ngrams
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import Normalizer
+from sklearn.pipeline import Pipeline
+from ..preprocessors.text_cleaner import TextCleaner
 
+
+def punctuation_ngrams():
+    pprorcessor = TextCleaner(filter_urls=True,
+                              filter_mentions=True,
+                              filter_hashtags=True,
+                              only_punctuation=True,
+                              lowercase=False)
+    vectorizer = CountVectorizer(min_df=1,
+                                 preprocessor=pprcessor,
+                                 analyzer='char',
+                                 ngram_range=(10, 10))
+    pipeline = Pipeline([('vect', vectorizer),
+                         ('tfidf', TfidfTransformer(sublinear_tf=True)),
+                         ('scale', Normalizer())])
+    punctuation_ngrams = ('punctuation_ngrams', pipeline)
+    return punctuation_ngrams
