480133b3b438b518bd20616dbecf3ce9d5c56428
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-04-13 15:56:00 +0200

    updated the script

--- profiler.sh
+++ profiler.sh
@@ -1,3 +1,36 @@
-echo Starting evaluation...
-make build
-docker exec profiler16_un /bin/sh -c "echo "hi""
+#!/bin/bash
+# Use > 1 to consume two arguments per pass in the loop (e.g. each
+# argument has a corresponding value to go with it).
+# Use > 0 to consume one or more arguments per pass in the loop (e.g.
+# some arguments don't have a corresponding value to go with it such
+# as in the --default example).
+# note: if this is set to > 0 the /etc/hosts part is not recognized ( may be a bug )
+while [[ $# > 1 ]]
+do
+key="$1"
+
+case $key in
+    -c|--corpus)
+    CORPUS="$2"
+    shift # past argument
+    ;;
+    -r|--run)
+    RUN="$2"
+    shift # past argument
+    ;;
+    -o|--output)
+    OUTPUT="$2"
+    shift # past argument
+    ;;
+    --default)
+    DEFAULT=YES
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+echo Input corpus    = "${CORPUS}"
+echo Input run       = "${RUN}"
+echo Output dir      = "${OUTPUT}"
