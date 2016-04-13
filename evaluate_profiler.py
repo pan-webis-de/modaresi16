7b06a9e6e52d2619620dd3abacef6be9e20497b7
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-04-13 16:31:23 +0200

    changed classifier to logistic regression

--- evaluate_profiler.py
+++ evaluate_profiler.py
@@ -33,7 +33,7 @@ def configure(conf):
     def build_pos_ngram_profiler(**args):
         return POSNGramProfiler(**args)
 
-    @conf.profiler('en_gender_profiler', lang='en', method='gradient_boosting')
+    @conf.profiler('en_gender_profiler', lang='en', method='logistic_regression')
     def build_en_gender_profiler(**args):
         return EnglishGenderProfiler(**args)
 
