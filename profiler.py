e2d5503e1606e40c63cbb9c20019138922c2e63e
Pashutan Modaresi <p.modaresi@gmx.de>
2016-04-15 14:33:46 +0200

    fixed bugs in volume mapping

--- profiler.py
+++ profiler.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import argparse
+import logging
 
 
 if __name__ == '__main__':
@@ -8,18 +9,18 @@ if __name__ == '__main__':
     argparser.add_argument('-l', '--log-level', dest='log_level', type=str, default='INFO',
                            help='Set log level (DEBUG, INFO, ERROR)')
 
-    argparser.add_argument('-i', '--input', dest='tira_input', type=str, required=True,
+    argparser.add_argument('-i', '--tira_input', dest='tira_input', type=str, required=True,
                            help='Path to the corpus for which the gender and age of the authors have to be predicted')
 
-    argparser.add_argument('-r', '--RUN', dest='input_run', type=str, required=False,
+    argparser.add_argument('-r', '--tira_run', dest='input_run', type=str, required=False,
                            help='Input run')
 
-    argparser.add_argument('-o', '--output', dest='tira_output', type=str, required=True,
+    argparser.add_argument('-o', '--tira_output', dest='tira_output', type=str, required=True,
                            help='Output directory')
 
     args = argparser.parse_args()
     LOGFMT = '%(asctime)s %(name)s %(levelname)s %(message)s'
     logging.basicConfig(level=getattr(logging, args.log_level), format=LOGFMT)
-    print(tira_input)
-    print(inptu_run)
-    print(tira_output)
+    print(args.tira_input)
+    print(args.input_run)
+    print(args.tira_output)
--- run.sh
+++ run.sh
@@ -22,7 +22,9 @@ VOLUMES=" -v $INPUT:/media/input -v $OUTPUT:/media/output  "
 VARS=" -e TIRA_INPUT=/media/input -e TIRA_OUTPUT=/media/output "
 IMAGE=profiler16_un
 CMD=""
+name=profiler16_un
+registry=hub.docker.com
 echo "[BEGIN DOCKER COMMAND]"
-docker run -it --rm=true $VOLUMES $VARS $IMAGE python profiler --tira_input /media/input --tira_output /media/output
-docker run -it --rm=true $VOLUMES $VARS $IMAGE chown -R 1000:1000 /media/output
+docker run -it --rm=true $VOLUMES $VARS $registry/$name python profiler.py --tira_input /media/input --tira_output /media/output
+docker run -it --rm=true $VOLUMES $VARS $registry/$name chown -R 1000:1000 /media/output
 echo "[END DOCKER COMMAND]"
