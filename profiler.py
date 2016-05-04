50f665bad116ebf3b0681750f76759ea85c085c0
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-05-04 11:49:16 +0200

    added funtionality to read and export tira data sets

--- profiler.py
+++ profiler.py
@@ -4,6 +4,40 @@ import logging
 from profiler16_un.datasets.pan_utils import load_xml_dataset, save_output_xmls
 from profiler16_un.profilers.en_gender_profiler import EnglishGenderProfiler
 
+base = '/media/training-datasets/author-profiling'
+en_corpus = base + '/pan16-author-profiling-training-dataset-english-2016-04-25'
+es_corpus = base + '/pan16-author-profiling-training-dataset-english-2016-04-25'
+nl_corpus = base + '/pain16-author-profiling-training-dataset-english-2016-04-25'
+
+
+def main(tira_input=None):
+    X_test, Y_test = load_xml_dataset(tira_input)
+    lang = X_test[0]['attr.lang'].lower()
+
+    if 'en' == lang:
+        X_train, Y_train = load_xml_dataset(en_corpus)
+
+    if 'es' == lang:
+        X_train, Y_train = load_xml_dataset(es_corpus)
+
+    if 'nl' == lang:
+        X_train, Y_train = load_xml_dataset(nl_corpus)
+
+    X_train = [x['text'] for x in X_train]
+    Y_train_gender = [y['gender'] for y in Y_train]
+    Y_train_age = [y['age_group'] for y in Y_train]
+
+    p = EnglishGenderProfiler(method='logistic_regression')
+    p.train(X_train, Y_train_gender)
+    Y_pred_gender = p.predict(X_test)
+
+    p = EnglishGenderProfiler(method='logistic_regression')
+    p.train(X_train, Y_train_age)
+    Y_pred_age = p.predict(X_train)
+
+    Y_pred = [{'gender': y_pred_gender, 'age_group': y_pred_age} for (y_pred_gender, y_pred_age) in zip(Y_pred_gender, Y_pred_age)]
+    save_output_xmls(args.tira_output, X_test, Y_pred)
+
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(
@@ -17,14 +51,7 @@ if __name__ == '__main__':
     argparser.add_argument('-o', '--tira_output', dest='tira_output', type=str, required=True,
                            help='Output directory')
 
-    p = EnglishGenderProfiler(method='logistic_regression')
     args = argparser.parse_args()
     LOGFMT = '%(asctime)s %(name)s %(levelname)s %(message)s'
     logging.basicConfig(level=getattr(logging, args.log_level), format=LOGFMT)
-    X, y = load_xml_dataset(args.tira_input)
-    texts = [x['text'] for x in X]
-    y = [yy['gender'] for yy in y]
-    p.train(texts, y)
-    y_pred_gender = p.predict(texts)
-    y_pred = [{'gender': y, 'age_group': '20-30'} for y in y_pred_gender]
-    save_output_xmls(args.tira_output, X, y_pred)
+    main(args.tira_input)
