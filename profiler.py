6a34d054f8d1686e5055b6da890b4f2c55f8f687
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-05-06 11:23:11 +0200

    defined features for age and gender

--- profiler.py
+++ profiler.py
@@ -14,26 +14,41 @@ def main(tira_input=None):
     X_test_txt = [x['text'] for x in X_test]
     lang = X_test[0]['attr.lang'].lower()
 
+    fns_gender = ['unigram', 'bigram', 'spelling', 'char']
+    fns_age = ['unigram', 'bigram', 'spelling', 'punctuation', 'char']
     if 'en' == lang:
         X_train, Y_train = load_xml_dataset(en_corpus)
+        X_train_txt = [x['text'] for x in X_train]
+        Y_train_gender = [y['gender'] for y in Y_train]
+        Y_train_age = [y['age_group'] for y in Y_train]
+        p_gender = EnglishGenderProfiler(lang='en', method='logistic_regression', feature_names=fns_gender)
+        p_gender.train(X_train_txt, Y_train_gender)
+        Y_pred_gender = p_gender.predict(X_test_txt)
+        p_age = EnglishGenderProfiler(lang='en', method='logistic_regression', feature_names=fns_age)
+        p_age.train(X_train_txt, Y_train_age)
+        Y_pred_age = p_age.predict(X_test_txt)
 
     if 'es' == lang:
-        X_train, Y_train = load_xml_dataset(es_corpus)
+        X_train, Y_train = load_xml_dataset(en_corpus)
+        X_train_txt = [x['text'] for x in X_train]
+        Y_train_gender = [y['gender'] for y in Y_train]
+        Y_train_age = [y['age_group'] for y in Y_train]
+        p_gender = EnglishGenderProfiler(lang='es', method='logistic_regression', feature_names=fns_gender)
+        p_gender.train(X_train_txt, Y_train_gender)
+        Y_pred_gender = p_gender.predict(X_test_txt)
+        p_age = EnglishGenderProfiler(lang='es', method='logistic_regression', feature_names=fns_age)
+        p_age.train(X_train_txt, Y_train_age)
+        Y_pred_age = p_age.predict(X_test_txt)
 
     if 'nl' == lang:
-        X_train, Y_train = load_xml_dataset(nl_corpus)
+        X_train, Y_train = load_xml_dataset(en_corpus)
+        X_train_txt = [x['text'] for x in X_train]
+        Y_train_gender = [y['gender'] for y in Y_train]
+        p_gender = EnglishGenderProfiler(lang='nl', method='logistic_regression', feature_names=fns_gender)
+        p_gender.train(X_train_txt, Y_train_gender)
+        Y_pred_gender = p_gender.predict(X_test_txt)
+        Y_pred_age = ['xx-xx' for y in Y_pred_gender]
 
-    X_train_txt = [x['text'] for x in X_train]
-    Y_train_gender = [y['gender'] for y in Y_train]
-    Y_train_age = [y['age_group'] for y in Y_train]
-
-    p = EnglishGenderProfiler(method='logistic_regression')
-    p.train(X_train_txt, Y_train_gender)
-    Y_pred_gender = p.predict(X_test_txt)
-
-    p = EnglishGenderProfiler(method='logistic_regression')
-    p.train(X_train_txt, Y_train_age)
-    Y_pred_age = p.predict(X_test_txt)
 
     Y_pred = [{'gender': y_pred_gender, 'age_group': y_pred_age} for (y_pred_gender, y_pred_age) in zip(Y_pred_gender, Y_pred_age)]
     save_output_xmls(args.tira_output, X_test, Y_pred)
