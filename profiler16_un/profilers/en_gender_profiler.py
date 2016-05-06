a62d84c8a0f19d9b5a164180864ac561b7e7a6ca
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-05-06 11:14:25 +0200

    added parameters to the main profiler

--- profiler16_un/profilers/en_gender_profiler.py
+++ profiler16_un/profilers/en_gender_profiler.py
@@ -1,29 +1,34 @@
 from ..pipelines.pipelines import avg_spelling_error
 from ..pipelines.pipelines import word_unigrams
-from sklearn.feature_selection import f_classif
 from ..pipelines.pipelines import word_bigrams
 from ..pipelines.pipelines import char_ngrams
 from ..pipelines.pipelines import avg_embeddings_count
 from ..pipelines.pipelines import punctuation_features
 from sklearn.preprocessing import Normalizer
-from sklearn.feature_selection import chi2
 from sklearn.pipeline import FeatureUnion
 from ..utils.utils import get_classifier
 from sklearn.pipeline import Pipeline
 
 
 class EnglishGenderProfiler():
-    def __init__(self, lang='en', method=None):
-        features = FeatureUnion([word_unigrams(),
-                                 # word_bigrams(),
-                                 # avg_spelling_error(lang=lang),
-                                 # avg_embeddings_count(lang=lang),
-                                 # punctuation_features()
-                                 # char_ngrams()
-                                 ], n_jobs=1)
+    def __init__(self, lang=None, method=None, feature_names=None):
+        fs = []
+        if 'unigram' in feature_names:
+            fs.append(word_unigrams())
+        if 'bigram' in feature_names:
+            fs.append(word_bigrams())
+        if 'spelling' in feature_names:
+            fs.append(avg_spelling_error(lang=lang))
+        if 'embedding' in feature_names:
+            fs.append(avg_embeddings_count(lang=lang))
+        if 'punctuation' in feature_names:
+            fs.append(punctuation_features())
+        if 'char' in feature_names:
+            fs.append(char_ngrams())
+
+        features = FeatureUnion(fs, n_jobs=1)
         self.pipeline = Pipeline([('features', features),
                                   ('scale', Normalizer()),
-                                  # ('chi', SelectKBest(f_classif, k=30000)),
                                   ('classifier', get_classifier(method=method))])
 
     def train(self, X_train, Y_train):
